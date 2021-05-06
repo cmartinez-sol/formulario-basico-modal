6c06461506596adde8f570e98d1f2a038f824f15
cmartinez-sol <cmartinez@solarig.com>
2021-05-06 12:11:21 +0200

    formulario livewire báasico con modal

---
+++ resources/views/livewire/create.blade.php
@@ -0,0 +1,42 @@
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+	Open Form
+</button>
+<!-- Modal -->
+<div wire:ignore.self class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" x-data="{show_hide: 1}">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true close-btn">×</span>
+                </button>
+            </div>
+           <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Name" wire:model="name">
+                        @error('name') <span class="text-danger error">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput2">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput2" wire:model="email" placeholder="Enter Email">
+                        @error('email') <span class="text-danger error">{{ $message }}</span>@enderror
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer" >
+                <select name="show_hide" x-model="show_hide" class="btn btn-primary close-modal">
+                    <option value="1">Show</option>
+                    <option value="2">Hide</option>
+                </select>
+                <button type="button" class="btn btn-secondary close-btn" data-dismiss="modal" x-show="show_hide == 1">Close</button>
+                <button type="button" wire:click.prevent="store()" class="btn btn-success" x-show="show_hide == 1">Save changes</button>
+
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
